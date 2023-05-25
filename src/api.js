e2c04e9ac68bc54e0a43ccb69fdb280b77a8241b
Yohann <115029585+GessyBoy@users.noreply.github.com>
2023-05-25 14:31:21 +0200

    Update api.js
    visibility Bearer

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   headers: {
-    "Authorization": "Bearer 6q4x2b1wpen5v1rxkvu3u2bp2atto2",
+    "Authorization": `Bearer ${{ secrets.MY_TOKEN }}`,
     "Client-ID": "c7zl793bbqnq3vqb4dj2g2f5l77ty7",
   }
 });
